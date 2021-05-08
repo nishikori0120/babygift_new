c3273504d0714f845c08d6805e8396ca15e50117
nishikori0120 <73696985+nishikori0120@users.noreply.github.com>
2021-05-08 22:20:32 +0900

    Userモデルとテーブルの作成

--- .history/db/migrate/20210508130847_devise_create_users_20210508220846.rb
+++ .history/db/migrate/20210508130847_devise_create_users_20210508220846.rb
@@ -4,12 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :first_name,         null: false
-      t.string :last_name,          null: false
-      t.string :first_name_kana,    null: false
-      t.string :last_name_kana,     null: false
       t.string :email,              null: false, default: ""
-      t.string :tel,                null: false
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
