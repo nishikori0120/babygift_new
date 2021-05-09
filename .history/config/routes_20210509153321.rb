a9aa40a90971efae19b226c7c2b3eabf881592fe
nishikori0120 <73696985+nishikori0120@users.noreply.github.com>
2021-05-09 15:56:53 +0900

    ヘッダー実装

--- .history/config/routes_20210509153321.rb
+++ .history/config/routes_20210509153321.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
     registrations: 'users/registrations'
   }
   devise_scope :user do
+    # post 'addresses', to: 'users/registrations#create'
     post 'addresses', to: 'users/registrations#create_address'
     # patch 'users', to: 'users/registrations#update'
     # patch 'addresses', to: 'users/registrations#address_update'
   end
   root to: 'orders#index'
-  resources :orders, only: [:new, :create,]
+
   resources :users, only: [:show, :edit]
 end
