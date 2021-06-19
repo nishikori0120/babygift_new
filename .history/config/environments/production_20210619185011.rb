f273e1c9f82515f7fa3be5cdf7b2e9036f6c1580
nishikori0120 <73696985+nishikori0120@users.noreply.github.com>
2021-06-19 18:51:12 +0900

    メールアドオンの変更

--- .history/config/environments/production_20210619185011.rb
+++ .history/config/environments/production_20210619185011.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'babygiftnew-01.herokuapp.com'
+  host = '.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
      :port => ENV['MAILGUN_SMTP_PORT'],
